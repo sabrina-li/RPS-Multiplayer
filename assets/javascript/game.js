3dac789fd927b4c672ea3de1b1d715caf26e8af1
Sabrina L <sabrinali0312@gmail.com>
2019-03-20 23:06:15 -0400

    fix aspect ratio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,16 +47,30 @@ function selectHand(svgObject,hand){
     //center this hand
     console.log("obj:", svgObject);
     var handPos = hand.getBoundingClientRect();
-    var boxPos=document.getElementById("innersvg").getBoundingClientRect();
+    var boxPos=document.getElementById("myselection").getBoundingClientRect();
     // console.log(svgObject);
     
     // console.log(handPos.top, handPos.height/2);
     // console.log(boxPos.height/2);
     svgObject.style.transform=`translate(${boxPos.width/2-handPos.left-handPos.width/2}px,
                                      ${boxPos.height/2-handPos.top-handPos.height/2}px)`;
+    
+    let elements = svgObject.contentDocument.getElementsByClassName("arrow")
+    for(var i=0;i<elements.length;i++){  
+        elements[i].setAttribute("style","display:none");
+    }
+    let hands = svgObject.contentDocument.getElementsByClassName("hand")
+    for(var i=0;i<hands.length;i++){  
+        if(hands[i].id !== hand.id){
+            hands[i].setAttribute("style","display:none");
+        }
+        
+    }
+    //svgObject.getElementById("rock").style.display="none"
+    //svgObject.getElementsByClassName("arrow").style.display="none"
                                    
-    svgObject.style.transformOrigin=`${boxPos.top + boxPos.height/2}px ${boxPos.left+boxPos.width/2}px`;
-    svgObject.style.transform ="scale(2)";
+    // svgObject.style.transformOrigin=`${boxPos.top + boxPos.height/2}px ${boxPos.left+boxPos.width/2}px`;
+    // svgObject.style.transform ="scale(2)";
     //svgObject.classList.add("zoomInHand");
     // console.log("obj:", svgObject.style);
     // console.log("hand",hand.id);
